11047586897cd495a5b19d0c9601177cde69eee5
tisons <salim.tison@fr.clara.net>
2026-02-27 16:54:40 +0100

    bouton retour amelioration page serie

--- src/views/pages/ShowDetailPage/ShowDetailPage.tsx
+++ src/views/pages/ShowDetailPage/ShowDetailPage.tsx
@@ -95,8 +95,10 @@ export const ShowDetailPage = () => {
 
   return (
     <div className="show-detail-page">
-      <button className="show-detail-page__btn-back" onClick={handleBack}>
-        ← Retour
+      <button className="show-detail-page__btn-back" onClick={handleBack} aria-label="Retour">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M19 12H5M12 19l-7-7 7-7" />
+        </svg>
       </button>
 
       <div className="show-detail-page__header">
